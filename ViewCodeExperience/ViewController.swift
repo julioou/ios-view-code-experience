692e31e314740bd4794bd1223c46a6eae96bd9f4
Júlio Cesar de Assis Santos <julioou.assis@gmail.com>
2020-08-03 22:27:43 -0300

    first commit

--- ViewCodeExperience/ViewController.swift
+++ ViewCodeExperience/ViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    lazy var wallet: WalletView = {
+       let view = WalletView()
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        wallet.show()       
     }
-
-
 }
 
