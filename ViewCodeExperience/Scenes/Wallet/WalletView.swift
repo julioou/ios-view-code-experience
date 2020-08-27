a280e42f85d04b1d4007d4352a1f09006c88520e
julioou <personal@mail.com>
2020-08-27 06:57:03 -0300

    Add FeatureBaseView

--- ViewCodeExperience/Scenes/Wallet/WalletView.swift
+++ ViewCodeExperience/Scenes/Wallet/WalletView.swift
@@ -12,33 +12,40 @@ import SnapKit
 import ChameleonFramework
 //import Colours
 
-class WalletView: UIView, ViewCodeProtocol {
+class WalletView: UIView {
+// MARK: PROPERTIES
     lazy var navigationBar: UINavigationBar = {
         let navBar = UINavigationBar.instantiateNavigationBar(onTopOf: superview)
         return navBar
     }()
 
-    lazy var notificationBox: UIView = {
+    var notificationBox: UIView = {
         let box = UIView()
         return box
     }()
 
-    lazy var notificationIcon: UIImageView = {
+    var notificationIcon: UIImageView = {
         let imageView = UIImageView()
         return imageView
     }()
 
-    lazy var cardHeader: UIView = {
+    var cardHeader: UIView = {
        let view = UIView()
             return view
     }()
 
-    lazy var walletCard: UIView = {
+    var walletCard: UIView = {
         let view = UIView()
         return view
     }()
 
-    init() {
+    var image: UIImage
+
+    var featureBox: FeatureBaseView {
+        return FeatureBaseView(image: image, title: "Send Money", detail: "Instant fund transfer between bank accounts")
+    }
+    init(image: UIImage) {
+        self.image = image
         super.init(frame: .zero)
     }
 
@@ -46,15 +53,30 @@ class WalletView: UIView, ViewCodeProtocol {
         fatalError("init(coder:) has not been implemented")
     }
 
+//
+//    func setImage(for imageView: UIImageView, withResource resource: String, ofType type: String) {
+//        if let path = Bundle.main.path(forResource: resource, ofType: type) {
+//            imageView.image = UIImage(contentsOfFile: path)
+//        }
+//    }
+    func setCardStyle() {
+
+    }
+
+    func setFeatureButtonStyle() {
+
+    }
+
+}
+
+// MARK: - VIEWCODEPROTOCOLS METHODS
+extension WalletView: ViewCodeProtocol {
     func show() {
-        UIApplication.shared.keyWindow?.rootViewController?.view.addSubview(self)
-        self.snp.makeConstraints { (make) in
-            make.edges.equalToSuperview()
-        }
         setUp()
     }
 
     func setHierarchy() {
+        UIApplication.shared.keyWindow?.rootViewController?.view.addSubview(self)
         addSubview(navigationBar)
         addSubview(walletCard)
         addSubview(notificationBox)
@@ -62,16 +84,14 @@ class WalletView: UIView, ViewCodeProtocol {
     }
 
     func setConstraints() {
+        self.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+
         walletCard.snp.makeConstraints { (make) in
             make.left.right.equalToSuperview().inset(40)
             make.top.equalTo(navigationBar.snp.bottom).offset(20)
             make.bottom.equalTo(self.snp.centerY)
-        }
-    }
-
-    func setImage(for imageView: UIImageView, withResource resource: String, ofType type: String) {
-        if let path = Bundle.main.path(forResource: resource, ofType: type) {
-            imageView.image = UIImage(contentsOfFile: path)
         }
     }
 
